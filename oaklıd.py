0da69d480f17c40df1351a646c133fafb40503e4
zehrossh <fatmaz.adgzl@gmail.com>
2024-09-13 22:27:57 +0300

    Add files via upload

---
+++ oaklıd.py
@@ -0,0 +1,14 @@
+import math
+
+points = [(1,2),(3,4),(5,6),(7,8)]
+def euclideanDistance(point1, point2):
+    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+
+print(f"Minimum mesafe: {min_distance}")
